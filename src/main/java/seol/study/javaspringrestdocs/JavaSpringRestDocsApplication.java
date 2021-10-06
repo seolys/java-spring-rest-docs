9f48c7ef823b54a24578a13f7c7ebadeeea8710d
설연수 <seolnavy@naver.com>
2021-10-06 19:47:03 +0900

    Initial commit

---
+++ src/main/java/seol/study/javaspringrestdocs/JavaSpringRestDocsApplication.java
@@ -0,0 +1,13 @@
+package seol.study.javaspringrestdocs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringRestDocsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringRestDocsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/seol/study/javaspringrestdocs/JavaSpringRestDocsApplicationTests.java
@@ -0,0 +1,13 @@
+package seol.study.javaspringrestdocs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringRestDocsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
